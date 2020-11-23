b69edf6e4fce701b56337e2f87bec436d55d334e
Michael <micjanic20@gmail.com>
2020-11-22 22:44:48 -0600

    set up start

---
+++ script.js
@@ -0,0 +1,75 @@
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+let particleArray = []
+
+//handle mouse
+const mouse = {
+    x: null,
+    y: null,
+    radius: 150,
+}
+
+window.addEventListener('mousemove', function ({ x, y }) {
+    mouse.x = x
+    mouse.y = y
+})
+
+ctx.fillStyle = 'white'
+ctx.font = '30px Verdana'
+ctx.fillText('A', 0, 30)
+const data = ctx.getImageData(0, 0, 100, 100)
+
+class Particle {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+        this.size = 3
+        this.baseX = this.x
+        this.baseY = this.y
+        //prettier-ignore
+        this.density = (Math.random() * 30) + 1
+    }
+    draw() {
+        ctx.fillStyle = 'red'
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+        ctx.closePath()
+        ctx.fill()
+    }
+    update() {
+        let dx = mouse.x - this.x
+        let dy = mouse.y - this.y
+        let distance = Math.hypot(dx, dy)
+
+        if (distance < 100) {
+            this.size = 50
+        } else {
+            this.size = 3
+        }
+    }
+}
+
+function init() {
+    particleArray = []
+    for (let i = 0; i < 1000; i++) {
+        let x = Math.random() * canvas.width
+        let y = Math.random() * canvas.height
+        particleArray.push(new Particle(x, y))
+    }
+    particleArray.push(new Particle(50, 90))
+}
+
+init()
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0; i < particleArray.length; i++) {
+        particleArray[i].draw()
+        particleArray[i].update()
+    }
+    requestAnimationFrame(animate)
+}
+
+animate()
